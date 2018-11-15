b925a5864a69ac6acd491df0b3f29247bde218da
kth <thxogusrla@naver.com>
2018-11-15 19:30:33 +0900

    11.15 과제1

---
+++ 과제 1.py
@@ -0,0 +1,15 @@
+"""A"""
+money = input("얼마를 가지고 계신가요 ?")
+print("입력한값 %s 입니다." %money)
+"""B"""
+s = "가나다라마\n바사아자차카차파하"
+
+"""c"""
+name = input("이름을 입력하시오:")
+print(name*10)
+
+"""D"""
+age = int(input("나이를 입력하시오:"))
+s_name = input("이름을 입력하시오:")
+str(age)
+print("나의 이름은 %s 이고, 나이는 %s입니다." %(s_name,age))
